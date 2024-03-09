3ea1095a9c2b50b1d89b586e48608115d319f97b
seoyun03 <jangs370@newschool.edu>
2024-03-08 22:15:46 -0500

    update

--- filmstrip/script.js
+++ filmstrip/script.js
@@ -1,25 +1,23 @@
-let toggles = document.querySelectorAll('.toggle')
-let gridItems = document.querySelectorAll('.grid-item')
+let toggles=document.querySelectorAll('.toggle')
+let gridItems= document.querySelectorAll('.grid-item')
 
 toggles.forEach((toggle, index) => {
-    let toggleType = toggle.dataset.type
+  let toggleType = toggle.dataset.type
 
-    toggle.addEventListener('click', (e) =>{
-        gridItems.forEach((item, index) => {
-            if (item.dataset.type == toggleType){
-                item,classList.remove('hidden')
-            } else{
-                    item.classList.add('hidden')
-                }
-            })
-        })
+
+  toggle.addEventListener('click', (e) => {
+    gridItems.forEach((item, index) => {
+      if (item.dataset.type== toggleType){
+        item.classList.remove('hidden')
+
+      } else{
+        item.classList.add('hidden')
+      }
+    
+
     })
 
-toggles.forEach((element, index) => {
-    console.log('element', index); 
-    console.log('index', index); 
-})
+  })
 
-natureBtn.addEventListener('click', () => {
-    console.log('')
+
 })
